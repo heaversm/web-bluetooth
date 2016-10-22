121b1a7dc42c9f0c79673bb6f50a6e3e3f5f2c8b
Mike Heavers <mheavers@gmail.com>
2016-10-22 13:03:32 -0400

    add example where you can list one of any of the minimum filters to scan for available bluetooth devices, and log to the screen basic properties about the connected device

---
+++ params/main.js
@@ -0,0 +1,38 @@
+document.querySelector('button').addEventListener('click',onButtonClick);
+
+function onButtonClick() {
+  let filters = [];
+
+  let filterService = document.querySelector('#service').value;
+  if (filterService.startsWith('0x')) {
+    filterService = parseInt(filterService);
+  }
+  if (filterService) {
+    filters.push({services: [filterService]});
+  }
+
+  let filterName = document.querySelector('#name').value;
+  if (filterName) {
+    filters.push({name: filterName});
+  }
+
+  let filterNamePrefix = document.querySelector('#namePrefix').value;
+  if (filterNamePrefix) {
+    filters.push({namePrefix: filterNamePrefix});
+  }
+
+  log('Requesting Bluetooth Device...');
+  navigator.bluetooth.requestDevice({filters: filters})
+  .then(device => {
+    log('> Name:             ' + device.name);
+    log('> Id:               ' + device.id);
+    log('> Connected:        ' + device.gatt.connected);
+  })
+  .catch(error => {
+    log('Argh! ' + error);
+  });
+}
+
+function log(msg){
+  document.querySelector('#log').innerHTML += msg + '<br/>';
+}
